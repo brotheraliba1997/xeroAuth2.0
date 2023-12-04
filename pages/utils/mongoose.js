277044d5e615c252eb79547d78f4ae2310493543
hamza ali <61055100+brotheraliba1997@users.noreply.github.com>
2023-12-04 10:31:57 +0500

    connect

--- pages/utils/mongoose.js
+++ pages/utils/mongoose.js
@@ -12,12 +12,12 @@ export async function dbConnect() {
   }
 
   const db = await connect(
-    process.env.MONGODB_URI || "mongodb://localhost:27017/nextjs"
+    process.env.MONGODB_URI || "mongodb+srv://hamzaali1997h:hamza2259@cluster0.uihexd9.mongodb.net/?retryWrites=true&w=majority"
   );
   // console.log(db.connection.db.databaseName);
   conn.isConnected = db.connections[0].readyState;
 }
-
+// mongodb+srv://hamzaali1997h:<password>@cluster0.uihexd9.mongodb.net/
 // connection.on("connected", () => console.log("Mongodb connected to db"));
 
 // connection.on("error", (err) => console.error("Mongodb Errro:", err.message));
